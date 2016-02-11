249350b3769478478c1c2696a72d927278d7c858
Buck Baskin <mobile.wbaskin@gmail.com>
2016-02-10 21:50:43 -0500

    Matrix Math example EKF w/ known corresp.

---
+++ examples/EKF_known_corr/EKF_known_corr.ino
@@ -0,0 +1,45 @@
+#include <MatrixMath.h>
+#include <rate.h>
+
+#define N  (5)
+/*
+ * State = x, y, heading, linear vel, angular vel
+ */
+float last_mean[N] = {0};
+float last_covar[N][N] = {0};
+
+Rate rate(10.0);
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  for (int i = 0; i < N; i++) {
+    last_covar[i][i] = .1;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  rate.sleep();
+}
+
+void ekf_known_correspondences(float* last_mean, float* last_covar, float* ctrl, float* mes, int* cor, float* m) {
+  // mu hat
+  float temp[N];
+  // TODO calc temp from control
+  float temp_mean[N];
+  Matrix.Add((float*) last_mean, (float*) temp, N, N, (float*) temp_mean);
+
+  // jacobian of motion model G
+  float G[N][N] = {0};
+
+  // sigma hat
+  float temp_sigma[N][N];
+  float temp2[N][N];
+  float R[N][N];
+  Matrix.Multiply((float*) G, (float*) last_covar, N, N, N, (float*) temp2);
+  Matrix.Transpose((float*) G, N, N, (float*) G);
+  Matrix.Multiply((float*) temp2, (float*) G, N, N, N, (float*) temp2);
+  Matrix.Add((float*) temp2, (float*) R, N, N, (float*) temp_sigma);
+}
